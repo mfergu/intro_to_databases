8d3e1e7c4ff8a0267d8d15211adfbf410e675441
mfergu <mfergu3@g.clemson.edu>
2017-06-07 09:54:02 -0400

    Create part3_load_tables.sql

---
+++ part3_load_tables.sql
@@ -0,0 +1,28 @@
+LOAD DATA LOCAL INFILE 'D:\\Users\\WOJTEK\\Documents\\school\\summer_2017\\4620\\data_customer.txt'
+	INTO TABLE CUSTOMER
+	FIELDS TERMINATED BY ','
+	LINES TERMINATED BY '\r\n';
+	
+LOAD DATA LOCAL INFILE 'D:\\Users\\WOJTEK\\Documents\\school\\summer_2017\\4620\\data_topping.txt'
+	INTO TABLE TOPPINGS
+	FIELDS TERMINATED BY ','
+	LINES TERMINATED BY '\r\n';
+	
+LOAD DATA LOCAL INFILE 'D:\\Users\\WOJTEK\\Documents\\school\\summer_2017\\4620\\data_base-price.txt'
+	INTO TABLE BASE_PRICES
+	FIELDS TERMINATED BY ','
+	LINES TERMINATED BY '\r\n';
+
+LOAD DATA LOCAL INFILE 'D:\\Users\\WOJTEK\\Documents\\school\\summer_2017\\4620\\data_discount.txt'
+	INTO TABLE DISCOUNTS
+	FIELDS TERMINATED BY ','
+	LINES TERMINATED BY '\r\n';
+	
+	/*
+	SELECT *   
+	FROM TOPPINGS;
+	
+	DELETE CUSTOMER 
+	FROM CUSTOMER
+	WHERE Cust_id=0;
+	*/
